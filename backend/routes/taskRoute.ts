8ddc581b01591663f44abc55ffbc655396042f91
Christian Hart <christiantaylorhart@gmail.com>
2023-10-26 15:32:08 -0400

    Use http-status-codes package in taskRoute.js

--- backend/routes/taskRoute.ts
+++ backend/routes/taskRoute.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import taskService from '../services/taskService.js';
 const router = Router();
 
@@ -6,34 +7,36 @@ router.route('/').get(async (req, res) => {
   //   const response = getAllTasks();
 
   const tasks = await taskService.getAllTasks();
-  res.status(200).send(tasks);
+  res.status(StatusCodes.OK).send(tasks);
 });
 
 router.route('/').post(async (req, res) => {
   console.log(req.body);
   if (!req.body.name) {
-    res.status(400).send('You must include a name for the new task.');
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .send('You must include a name for the new task.');
     return;
   }
   const task = await taskService.createTask(req.body.name, req.body.completed);
-  res.status(200).send(task);
+  res.status(StatusCodes.OK).send(task);
 });
 
 router.route('/:id').patch(async (req, res) => {
   const task = await taskService.modifyTask(req.params.id, req.body);
   if (!task) {
-    res.status(404).send({ msg: 'Not found' });
+    res.status(StatusCodes.NOT_FOUND).send({ msg: 'Not found' });
     return;
   }
-  res.status(200).send({ msg: 'success', task });
+  res.status(StatusCodes.OK).send({ msg: 'success', task });
 });
 router.route('/:id').delete(async (req, res) => {
   const task = await taskService.deleteTask(req.params.id);
   if (!task) {
-    res.status(404).send({ msg: 'No task to delete' });
+    res.status(StatusCodes.NOT_FOUND).send({ msg: 'No task to delete' });
     return;
   }
-  res.status(200).send({ msg: 'Task deleted successfully' });
+  res.status(StatusCodes.OK).send({ msg: 'Task deleted successfully' });
 });
 
 export default router;
